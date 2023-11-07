fe262e02c2085b10466a9d586b523415aa215353
copecone <controld3785@gmail.com>
2023-11-07 20:11:50 +0900

    Add Sample Code

--- server/src/main/kotlin/io/github/forceload/sample/SampleMod.kt
+++ server/src/main/kotlin/io/github/forceload/sample/SampleMod.kt
@@ -1,9 +1,28 @@
 package io.github.forceload.sample
 
+import com.mojang.logging.LogUtils
+import net.fabricmc.api.EnvType
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.client.MinecraftClient
 
 object SampleMod: ModInitializer {
+    const val MOD_ID = "sample"
+    private lateinit var loaderInstance: FabricLoader
+    lateinit var clientInstance: MinecraftClient
+
+    val logger = LogUtils.getLogger()
+
     override fun onInitialize() {
-        TODO("Not yet implemented")
+        loaderInstance = FabricLoader.getInstance()
+        clientInstance = MinecraftClient.getInstance()
+
+        when (loaderInstance.environmentType) {
+            EnvType.SERVER -> {
+                TODO("Write Your Code")
+            }
+
+            else -> logger.error("This mode is only supported on the server!")
+        }
     }
 }
